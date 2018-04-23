8f68091041e6352cfe3b91f7cc55a03a6020af0a
Giuseppe Parodi <g.parodi@arol.it>
2018-04-23 10:20:33 +0200

    Issue #8: Aggiunta funzione per gestione della modifica del colore di sfondo di una riga del datagridview
    1) Aggiunto il file DataGridViewFunctions.vb.
    2) Aggiunta la funzione fChangeRowColor, che permette di modificare il colore di sfondo di una riga di un datagridview, in funzione dello stato del relativo codice Arol. La funzione riceve in ingresso il datagridview da modificare, l'indice della riga e l'indice intero del colore relativo.

---
+++ WHExplorer/Functions/DataGridViewFunctions.vb
@@ -0,0 +1,25 @@
+﻿Module DataGridViewFunctions
+    Public Function fChangeRowColor(datagridView As DataGridView, rowIndex As Integer, colorIndex As Integer) As Boolean
+        Try
+            Dim selectedColor As Color
+            Select Case colorIndex
+                Case 0
+                    selectedColor = Color.FromArgb(204, 255, 204)
+                Case 1
+                    selectedColor = Color.FromArgb(255, 255, 153)
+                Case 2
+                    selectedColor = Color.FromArgb(255, 214, 153)
+                Case 3
+                    selectedColor = Color.FromArgb(255, 173, 153)
+                Case Else
+                    selectedColor = Nothing
+            End Select
+            datagridView.Rows(rowIndex).DefaultCellStyle.BackColor = selectedColor
+            Return True
+        Catch ex As Exception
+            MsgBox("ERRORE NEL CAMBIO COLORE DI SFONDO RIGA DEL DATAGRIDVIEW", MsgBoxStyle.Critical)
+            fAddLogRow(frmMain.strLogFilePath, "Utente: " & ex.ToString)
+            Return False
+        End Try
+    End Function
+End Module

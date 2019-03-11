4af5fc3dcf592ef927f20fddcd620899dac1c5cf
Unknown <g.parodi@arol.it>
2019-03-11 13:56:48 +0100

    Aggiornato indice di revisione
    Nella finestra "About", aggiornati indice di revisione e data di pubblicazione.

--- WHExplorer/frmMain.vb
+++ WHExplorer/frmMain.vb
@@ -412,8 +412,8 @@ Public Class frmMain
         fFindElement()
     End Sub
     Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
-        frmAbout.lblRevision.Text = "Xplorer 1.2.7.11"
-        frmAbout.lblIssuedDate.Text = "09/03/2019"
+        frmAbout.lblRevision.Text = "Xplorer 1.2.8.12"
+        frmAbout.lblIssuedDate.Text = "11/03/2019"
         frmAbout.TopMost = True
         frmAbout.Show()
     End Sub
